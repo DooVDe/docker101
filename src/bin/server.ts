cfc4b7813f699be3830421496667b8aa23a25b9b
David Litvak <dvd.net@gmail.com>
2020-11-03 20:23:17 +0200

    removed .gitkeep

--- src/bin/server.ts
+++ src/bin/server.ts
@@ -6,17 +6,18 @@ import { readFileSync } from 'fs';
 import { promisify } from "util";
 import {hello} from '../lib/functions';
 
-// // Config
+// Config
 const HTTP_PORT: number = process.env.HTTP_PORT ? parseInt(process.env.HTTP_PORT) : 8080;
 const REDIS_HOST: string = process.env.REDIS_HOST ? process.env.REDIS_HOST : '127.0.0.1';
 
-// // Init
+// Defines
 const app = express();
 const client = redis.createClient({ host: REDIS_HOST });
 const indexHtml = readFileSync(path.join(__dirname, '..', '..', 'templates', 'index.html'), 'utf-8');
 const getAsync = promisify(client.get).bind(client);
 const setAsync = promisify(client.set).bind(client);
 
+// Logic
 app.get('/', async (req, res) => {
   const { set } = req.query;
   console.log('Got request!', (new Date()).toString());
@@ -37,36 +38,7 @@ app.get('/', async (req, res) => {
   res.send(template);
 });
 
+// Init
 app.listen(HTTP_PORT, () => {
-  hello(1);
   console.log(`Example app listening at http://localhost:${HTTP_PORT}`);
 });
-
-// interface User<T> {
-//     name: string,
-//     age: number,
-//     qwe: T
-// }
-
-// class AdvancedUser implements User<number> {
-//     public name:string;
-//     public age:number;
-//     public qwe:number;
-
-//     constructor(name:string, age:number, qwe:number) {
-//         this.name = name;
-//         this.age = age;
-//         this.qwe = qwe;
-//     }
-// }
-
-
-// export namespace Foo {
-//     // this coolest function ever
-//     export async function bar(str: User<number>) : Promise<number> {
-//         console.log('str is ', str)
-//         return 42
-//     }
-// }
-
-// Foo.bar(new AdvancedUser('hello', 32, 100));
