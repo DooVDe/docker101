214fb78202b23c7f1a11c51df83885464af97283
David Litvak <dvd.net@gmail.com>
2020-11-11 17:01:29 +0200

    Removed unnecessary files and dependencies

--- src/bin/server.ts
+++ src/bin/server.ts
@@ -1,41 +1,17 @@
 import express from 'express';
-import mustache from 'mustache';
-import path from 'path';
-import redis from 'redis';
-import { readFileSync } from 'fs';
-import { promisify } from "util";
-import {hello} from '../lib/functions';
 
 // Config
 const HTTP_PORT: number = process.env.HTTP_PORT ? parseInt(process.env.HTTP_PORT) : 8080;
-const REDIS_HOST: string = process.env.REDIS_HOST ? process.env.REDIS_HOST : '127.0.0.1';
 
 // Defines
 const app = express();
-const client = redis.createClient({ host: REDIS_HOST });
-const indexHtml = readFileSync(path.join(__dirname, '..', '..', 'templates', 'index.html'), 'utf-8');
-const getAsync = promisify(client.get).bind(client);
-const setAsync = promisify(client.set).bind(client);
 
 // Logic
 app.get('/', async (req, res) => {
-  const { set } = req.query;
-  console.log('Got request!', (new Date()).toString());
+  const date = new Date();
+  console.log(`Request@${date.toString()}`);
 
-  if (set === 'true') {
-    await setAsync('time', (new Date()).toString());
-  }
-
-  const params = {
-    nodeVersion: process.version,
-    httpPort: HTTP_PORT,
-    redisHost: REDIS_HOST,
-    redisSet: set === 'true' ? set : 'false',
-    time: await getAsync('time') || '---',
-  };
-
-  const template = mustache.render(indexHtml, params);
-  res.send(template);
+  res.send(`<h1>hello world</h1><pre>${date.toString()}</pre>`);
 });
 
 // Init
